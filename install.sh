fc4994f9bf43aba5646455f46931896b4e5a506b
bright98 <www.hhsshhss@gmail.com>
2023-06-24 16:53:19 +0330

    fix: install file

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 $(docker stop $(docker ps -a -q --filter ancestor=3x-ui-bot --format="{{.ID}}"))
-docker build -t 3x-ui-bot .
-docker run -d 3x-ui-bot
+# docker build -t 3x-ui-bot .
+# docker run -d 3x-ui-bot
+docker compose up --build -d 3x-ui-bot-service

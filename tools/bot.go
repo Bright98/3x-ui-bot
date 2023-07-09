17d44146604232a5871a4a17dbbee33ba420d67e
bright98 <www.hhsshhss@gmail.com>
2023-07-09 18:34:07 +0330

    try to fix bot token

--- tools/bot.go
+++ tools/bot.go
@@ -24,6 +24,7 @@ func init() {
 
 	//get bot token
 	botToken := os.Getenv(BotToken)
+	fmt.Println(botToken)
 
 	// create new bot
 	bot, err = telego.NewBot(botToken, telego.WithDefaultDebugLogger())

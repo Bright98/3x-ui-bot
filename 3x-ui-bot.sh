bfc62efea74d31d74ec2c2b3ca2df0606b9dd419
Bright98 <www.hhsshhss@gmail.com>
2023-11-27 11:17:22 +0330

    fix: directory problem

--- 3x-ui-bot.sh
+++ 3x-ui-bot.sh
@@ -1,11 +1,12 @@
-#!/bin/bash/
+#!/bin/bash
 
 # variables
-red='\033[0;31m'
-green='\033[0;32m'
-yellow='\033[0;33m'
-plain='\033[0m'
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+plain=$(tput sgr0)
 error_content='[ error happened! ]'
+installed_directory=/home/hsh/3x-ui-bot
 
 # helpers
 insert_in_yaml() {
@@ -15,14 +16,14 @@ insert_in_yaml() {
     local username="$3"
     local password="$4"
 
-    result=$(yq --yaml-output ".servers += [{"id": \""$id"\", "ip": \""$ip"\", "port": \""port"\", "username": \""$username"\", "password": \""$password"\"}]" servers.yaml)
+    result=$(yq --yaml-output ".servers += [{"id": \""$id"\", "ip": \""$ip"\", "port": "$port", "username": \""$username"\", "password": \""$password"\"}]" $installed_directory"/servers.yaml")
 
     if [ $? -ne 0 ]; then
         echo ${red}${error_content}
         return
     fi
 
-    echo "$result" > servers.yaml
+    echo "$result" > $installed_directory"/servers.yaml"
 }
 update_yaml() {
     local id="$1"
@@ -32,46 +33,53 @@ update_yaml() {
     local new_password="$5"
 
     command=$(yq --yaml-output \
-      ".servers |= map(if .id == \"$id\" then
-                        . + 
-                        {
-                          \"ip\": $(if [ -n "$new_ip" ]; then echo "\"$new_ip\""; else echo ".ip"; fi),
-                          \"port\": $(if [ -n "$new_port" ]; then echo "$new_port"; else echo ".port"; fi),
-                          \"username\": $(if [ -n "$new_username" ]; then echo "\"$new_username\""; else echo ".username"; fi),
-                          \"password\": $(if [ -n "$new_password" ]; then echo "\"$new_password\""; else echo ".password"; fi)
-                        }
-                      else
-                        .
-                      end)" \
-      servers.yaml)
+            ".servers |= map(if .id == \"$id\" then
+                . +
+                {
+                  \"ip\": $(if [ -n "$new_ip" ]; then echo "\"$new_ip\""; else echo ".ip"; fi),
+                  \"port\": $(if [ -n "$new_port" ]; then echo "$new_port"; else echo ".port"; fi),
+                  \"username\": $(if [ -n "$new_username" ]; then echo "\"$new_username\""; else echo ".username"; fi),
+                  \"password\": $(if [ -n "$new_password" ]; then echo "\"$new_password\""; else echo ".password"; fi)
+                }
+                else
+              .
+              end)" \
+            $installed_directory"/servers.yaml")
 
     if [ $? -ne 0 ]; then
         echo ${red}${error_content}
         return
     fi
 
-     echo "$command" > servers.yaml
+     echo "$command" > $installed_directory"/servers.yaml"
 }
 
 # menu functions
 set_bot_token() {
     echo && read -p "Please enter your telegram bot token: " token
-    command=$(yq --yaml-output '.services."3x-ui-bot".environment[] = "BotToken='$token'"' docker-compose.yaml)
+    command=$(yq --yaml-output '.services."3x-ui-bot".environment[] = "BotToken='$token'"' $installed_directory"/docker-compose.yaml")
 
     if [ $? -ne 0 ]; then
         echo ${red}${error_content}
         return
     fi
 
-    echo "$command" > docker-compose.yaml
+    echo "$command" > $installed_directory"/docker-compose.yaml"
+    echo
+    echo ${green}"-----------------"
     echo ${green}"[ Bot token set ]"
+    echo ${green}"-----------------"
 }
 get_servers() {
-    cat servers.yaml
+    echo
+    echo ${green}"-----------------"
+    cat $installed_directory"/servers.yaml"
     if [ $? -ne 0 ]; then
-	echo ${red}${error_content}
-	return
+        echo ${red}${error_content}
+        return
     fi
+
+    echo ${green}"-----------------"
 }
 define_new_server() {
     echo "Please enter your new server information: "
@@ -81,7 +89,10 @@ define_new_server() {
     echo -n && read -p "server password: " password
 
     insert_in_yaml $ip $port $username $password
+    echo
+    echo ${green}"-----------------"
     echo ${green}"[ New server added ]"
+    echo ${green}"-----------------"
 }
 update_server_info() {
     echo && read -p "server id: " id
@@ -91,13 +102,16 @@ update_server_info() {
     echo -n && read -p "server usernamse: " username
     echo -n && read -p "server password: " password
 
-    update_yaml $id $ip $port $username $password
+    update_yaml "$id" "$ip" "$port" "$username" "$password"
+    echo
+    echo ${green}"-----------------"
     echo ${green}"[ Server info updated ]"
+    echo ${green}"-----------------"
 }
 remove_server() {
     echo && read -p "Please enter your server id: " id
 
-    command='yq --yaml-output "del(.servers[] | select(.id == \""$id"\"))" servers.yaml'
+    command='yq --yaml-output "del(.servers[] | select(.id == \""$id"\"))" $installed_directory/servers.yaml'
     result="$(eval "$command")"
 
     if [ $? -ne 0 ]; then
@@ -105,18 +119,25 @@ remove_server() {
         return
     fi
 
-    echo "$result" > servers.yaml
+    echo "$result" > $installed_directory"/servers.yaml"
+    echo
+    echo ${green}"-----------------"
+    echo ${green}"[ Server removed ]"
+    echo ${green}"-----------------"
 }
 remove_all_servers() {
     content="servers:"
-    echo $content > servers.yaml
+    echo $content > $installed_directory"/servers.yaml"
 
     if [ $? -ne 0 ]; then
         echo ${red}${error_content}
         return
     fi
 
+    echo
+    echo ${green}"-----------------"
     echo ${green}"[ All servers removed ]"
+    echo ${green}"-----------------"
 }
 
 show_menu() {
@@ -141,20 +162,20 @@ show_menu() {
         set_bot_token
         ;;
     2)
-	get_servers
-	;;
+        get_servers
+        ;;
     3)
         define_new_server
-	;;
+        ;;
     4)
-	update_server_info
-	;;
+        update_server_info
+        ;;
     5)
-	remove_server
-	;;
+        remove_server
+        ;;
     6)
-	remove_all_servers
-	;;
+        remove_all_servers
+        ;;
     *)
         LOGE "Please enter the correct number [0-22]"
         ;;
--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
+blue='\033[0;34m'
 
 # Check if Python is installed
 if ! command -v python3 >/dev/null 2>&1; then
@@ -18,31 +19,29 @@ fi
 #fi
 
 # Stop old image and rebuild
-#echo ${yellow}"Stop running last 3x-ui-bot image..."
+#echo ${blue}"Stop running last 3x-ui-bot image..."
 #$(docker stop $(docker ps -a -q --filter ancestor=3x-ui-bot --format="{{.ID}}"))
 
-#echo ${yellow}"Installing 3x-ui-bot docker image..."
+#echo ${blue}"Installing 3x-ui-bot docker image..."
 #docker compose up --build -d 3x-ui-bot
 
 # Install Python dependencies using pip
-echo ${yellow}"Installing Python dependencies..."
+echo ${blue}"Installing Python dependencies..."
 pip3 install -r requirements.txt
 
 # Display Python and dependency versions
 python_version=$(python3 --version 2>&1)
 dependency_version=$(your_dependency --version 2>&1)
 
+# Replace installed directory
+this_path=$(pwd)
+command=$(sed "\|installed_directory=| s|.*|installed_directory=$this_path|" 3x-ui-bot.sh)
+echo "$command" > 3x-ui-bot.sh
+
 # Install 3x-ui-bot
-if [[ -e /usr/local/x-ui/ ]]; then
-    systemctl stop 3x-ui-bot
-    sudo rm /usr/local/3x-ui-bot/ -rf
-fi
-sudo cp 3x-ui-bot.sh /usr/local/bin/
-
+echo ${blue}"Installing 3x-ui bot..."
 chmod +x 3x-ui-bot.sh
-
-
-
+sudo cp 3x-ui-bot.sh /usr/local/bin/3x-ui-bot
 
 echo ${green}"Python version: $python_version"
 echo ${green}"Installation and version check completed successfully."
